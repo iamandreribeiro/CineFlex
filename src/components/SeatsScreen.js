b37fcf7d315f9148eedba6d3e3c14e3cf1300ee4
André <110317372+iamandreribeiro@users.noreply.github.com>
2022-10-27 00:08:55 -0300

    mudança na seleção dos assentos

--- src/components/SeatsScreen.js
+++ src/components/SeatsScreen.js
@@ -33,6 +33,9 @@ export default function SeatsScreen(props) {
         if (a.isAvailable && !props.numAssentos.includes(a.name)) {
             assentosId = [...props.idAssentos, a.id];
             assentosNum = [...props.numAssentos, a.name];
+        } else if (a.isAvailable && props.numAssentos.includes(a.name)){
+            assentosId.filter((x) => x != a.id);
+            assentosNum.filter((x) => x != a.name);
         } else alert("Esse assento não está disponível");
 
         props.setIdAssentos(assentosId);
